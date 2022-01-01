df68e627361a049c201d72ac2698d50130d664f6
Kirshin Maxim <42539356+meowto16@users.noreply.github.com>
2022-01-01 04:48:00 +0300

    Section 2.5. Types. Completed (#18)

---
+++ 1. JavaScript Language/2.5. Types/src/1-template-literals.js
@@ -0,0 +1,13 @@
+/**
+ * Шаблонные строки
+ *
+ * Что выведет этот скрипт?
+ */
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); // hello Ilya
