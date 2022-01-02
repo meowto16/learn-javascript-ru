dc6a93cebfbe46b027615ca18e659430205b9a09
Maxim Kirshin <mak@crtweb.ru>
2022-01-02 11:52:28 +0300

    Section 2.9. Comparison operators. Completed

---
+++ 1. JavaScript Language/2.9. Comparison/src/1-comparison-operators.js
@@ -0,0 +1,13 @@
+/**
+ * Операторы сравнения (важность: 5)
+ *
+ * Каким будет результат этих выражений?
+ */
+
+5 > 4 // true
+"ананас" > "яблоко" // false
+"2" > "12" // true
+undefined == null // true
+undefined === null // false
+null == "\n0\n" // false
+null === +"\n0\n" // false
