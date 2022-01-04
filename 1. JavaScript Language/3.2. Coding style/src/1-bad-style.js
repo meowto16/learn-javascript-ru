2c0d4e67473875f7ff7563da61fab4674ac3ea89
Maxim Kirshin <mak@crtweb.ru>
2022-01-04 14:39:03 +0300

    Section 3.2. Coding style. Completed

---
+++ 1. JavaScript Language/3.2. Coding style/src/1-bad-style.js
@@ -0,0 +1,43 @@
+/**
+ * Плохой стиль (важность: 4)
+ *
+ * Какие недостатки вы видите в стиле написания кода этого примера?
+ */
+
+function pow(x,n)
+{
+  let result=1;
+  for(let i=0;i<n;i++) {result*=x;}
+  return result;
+}
+
+let x=prompt("x?",''), n=prompt("n?",'')
+if (n<=0)
+{
+  alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`);
+}
+else
+{
+  alert(pow(x,n))
+}
+
+// Решение:
+
+function pow(x, n) {
+  let result = 1
+
+  for (let i = 0; i < n; i++) {
+    result *= x
+  }
+
+  return result
+}
+
+let x = prompt('x?', '')
+let n = prompt('n?', '')
+
+if (n <= 0) {
+  alert(`Степень ${n} не поддерживается, введите целую степень, большую 0`)
+} else {
+  alert(pow(x,n))
+}
