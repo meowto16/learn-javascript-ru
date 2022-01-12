6d3186d7c75760fae4a08f46f9a68a465cb3eb09
Kirshin Maxim <42539356+meowto16@users.noreply.github.com>
2022-01-13 03:35:34 +0500

    Section 5.8. WeakMap and WeakSet. Completed (#91)

---
+++ 1. JavaScript Language/5.8. WeakMap and WeakSet/src/1-store-read-info.js
@@ -0,0 +1,34 @@
+/**
+ * Хранение отметок "не прочитано" (важность: 5)
+ * Есть массив сообщений
+ * У вас есть к ним доступ, но управление этим массивом происходит где-то ещё.
+ * Добавляются новые сообщения и удаляются старые, и вы не знаете в какой момент это может произойти.
+ * Имея такую вводную информацию, решите, какую структуру данных вы могли бы использовать для ответа на
+ * вопрос «было ли сообщение прочитано?». Структура должна быть подходящей, чтобы можно было однозначно сказать, было ли прочитано это сообщение для каждого объекта сообщения.
+ * P.S. Когда сообщение удаляется из массива messages, оно должно также исчезать из структуры данных.
+ * P.P.S. Нам не следует модифицировать сами объекты сообщений, добавлять туда свойства.
+ * Если сообщения принадлежат какому-то другому коду, то это может привести к плохим последствиям.
+ */
+
+let messages = [
+  {text: "Hello", from: "John"},
+  {text: "How goes?", from: "John"},
+  {text: "See you soon", from: "Alice"}
+]
+
+const MessagesReadStatusBase = new function () {
+  this.set = new WeakSet()
+  this.readMessage = function (message) {
+    return this.set.add(message)
+  }
+  this.isMessageRead = function (message) {
+    return this.set.has(message)
+  }
+}
+
+MessagesReadStatusBase.readMessage(messages[0])
+MessagesReadStatusBase.readMessage(messages[1])
+
+MessagesReadStatusBase.isMessageRead(messages[1]) // true
+MessagesReadStatusBase.isMessageRead(messages[2]) // false
+
