e3382e6a026b7f7ca716d3d836d6c4bfbc488b34
Kirshin Maxim <42539356+meowto16@users.noreply.github.com>
2022-01-30 20:19:50 +0500

    Section 9.1. Class. Completed (#142)

---
+++ 1. JavaScript Language/9.1. Class/src/1-refactor-class.js
@@ -0,0 +1,85 @@
+/**
+ * Перепишите класс (важность: 5)
+ *
+ * Класс Clock написан в функциональном стиле. Перепишите его, используя современный синтаксис классов.
+ *
+ * P.S. Часики тикают в консоли. Откройте её, чтобы посмотреть.
+ */
+//
+// function Clock({ template }) {
+//
+//   let timer;
+//
+//   function render() {
+//     let date = new Date();
+//
+//     let hours = date.getHours();
+//     if (hours < 10) hours = '0' + hours;
+//
+//     let mins = date.getMinutes();
+//     if (mins < 10) mins = '0' + mins;
+//
+//     let secs = date.getSeconds();
+//     if (secs < 10) secs = '0' + secs;
+//
+//     let output = template
+//       .replace('h', hours)
+//       .replace('m', mins)
+//       .replace('s', secs);
+//
+//     console.log(output);
+//   }
+//
+//   this.stop = function() {
+//     clearInterval(timer);
+//   };
+//
+//   this.start = function() {
+//     render();
+//     timer = setInterval(render, 1000);
+//   };
+//
+// }
+//
+// let clock = new Clock({template: 'h:m:s'});
+// clock.start();
+
+class Clock {
+  timer = null
+  
+  constructor({ template }) {
+    this.template = template
+  }
+  
+  #render() {
+    let date = new Date();
+  
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+  
+    let mins = date.getMinutes();
+    if (mins < 10) mins = '0' + mins;
+  
+    let secs = date.getSeconds();
+    if (secs < 10) secs = '0' + secs;
+  
+    let output = this.template
+      .replace('h', hours)
+      .replace('m', mins)
+      .replace('s', secs);
+  
+    console.log(output);
+  }
+  
+  start() {
+    this.#render()
+    this.timer = setInterval(this.#render.bind(this), 1000)
+  }
+  
+  stop() {
+    clearInterval(this.timer)
+  }
+}
+
+let clock = new Clock({template: 'h:m:s'});
+clock.start();
