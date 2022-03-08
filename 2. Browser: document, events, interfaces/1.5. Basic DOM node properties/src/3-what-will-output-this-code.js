2bc3cf282cfc7913f27455d7ef46837843c7e7f1
Maxim Kirshin <meowto16@gmail.com>
2022-03-08 14:55:49 +0500

    Section 1.5. Basic DOM Node properties. Completed

---
+++ 2. Browser: document, events, interfaces/1.5. Basic DOM node properties/src/3-what-will-output-this-code.js
@@ -0,0 +1,15 @@
+/**
+ * Тег в комментарии (важность: 3)
+ *
+ * Что выведет этот код?
+ *
+ * <script>
+ *   let body = document.body;
+ *
+ *   body.innerHTML = "<!--" + body.tagName + "-->";
+ *
+ *   alert( body.firstChild.data ); // что выведет?
+ * </script>
+ */
+
+// Выведет BODY
