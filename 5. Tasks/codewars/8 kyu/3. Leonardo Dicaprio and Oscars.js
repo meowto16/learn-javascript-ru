be8dccfe9762cac9146c8ea3050f32f0c8eea5f0
Maxim Kirshin <meowto16@gmail.com>
2022-07-03 13:44:15 +0500

    Task: Leondardo Dicaprio and Oscars (8 kyu)

---
+++ 5. Tasks/codewars/8 kyu/3. Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,11 @@
+/**
+ * https://www.codewars.com/kata/56d49587df52101de70011e4
+ */
+
+function leo(oscar){
+    if (oscar === 88) return 'Leo finally won the oscar! Leo is happy'
+    if (oscar === 86) return 'Not even for Wolf of wallstreet?!'
+    if (oscar < 88) return 'When will you give Leo an Oscar?'
+
+    return 'Leo got one already!'
+}
