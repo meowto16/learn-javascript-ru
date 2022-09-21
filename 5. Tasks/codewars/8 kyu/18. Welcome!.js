365e712392acd970a47ed61878006dc411afd1c5
Maxim Kirshin <meowto16@gmail.com>
2022-09-21 23:52:59 +0500

    Task. Welcome (8 kyu). Completed

---
+++ 5. Tasks/codewars/8 kyu/18. Welcome!.js
@@ -0,0 +1,25 @@
+// https://www.codewars.com/kata/577ff15ad648a14b780000e7
+
+function greet(language) {
+  const i18n = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  }
+
+  return i18n[language] || i18n.english
+}
