24246451763529d9ec85ac70168a6e5d4d09cd41
Maxim Kirshin <meowto16@gmail.com>
2022-10-18 21:12:58 +0500

    Task. Add two integers (Leetcode easy). Completed

---
+++ 5. Tasks/leetcode/easy/0001. Add two integers.js
@@ -0,0 +1,9 @@
+/**
+ * https://leetcode.com/problems/add-two-integers
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+function sum(num1, num2) {
+  return num1 + num2;
+}
