2841857c328ae1e59e6b8a95fa11f0d1c946d406
Maxim Kirshin <meowto16@gmail.com>
2022-11-06 14:33:34 +0500

    Task. Spiralize (3 kyu). Almost completed

---
+++ 5. Tasks/codewars/3 kyu/2. Spiralize.js
@@ -0,0 +1,132 @@
+// https://www.codewars.com/kata/534e01fbbb17187c7e0000c6
+
+function spiralize(length) {
+  const result = Array.from({ length }, () => Array.from({ length }, () => 0))
+  spiralLoop(result, (i, j) => result[i][j] = 1)
+
+  return result
+}
+
+function spiralLoop(arr, callback) {
+  const size = arr.length
+
+  let direction = 'right'
+  let i = 0
+  let j = 0
+
+  const passed = new Map()
+
+  const setPassed = (i, j) => passed.set(`${i}${j}`, true)
+  const checkPassed = (i, j) => passed.get(`${i}${j}`) || false
+
+  const checkEnd = () => {
+    let around
+
+    if (direction === 'right') {
+      around = [
+        checkPassed(i - 1, j + 1),
+        checkPassed(i + 1, j + 1),
+        checkPassed(i, j + 1),
+      ]
+    }
+
+    if (direction === 'up') {
+      around = [
+        checkPassed(i - 1, j),
+        checkPassed(i - 1, j - 1),
+        checkPassed(i - 1, j + 1),
+      ]
+    }
+
+    if (direction === 'bottom') {
+      around = [
+        checkPassed(i + 1, j),
+        checkPassed(i + 1, j - 1),
+        checkPassed(i + 1, j + 1),
+      ]
+    }
+
+    if (direction === 'left') {
+      around = [
+        checkPassed(i, j - 1),
+        checkPassed(i - 1, j - 1),
+        checkPassed(i + 1, j - 1),
+      ]
+    }
+
+    console.log({ around, i, j, direction, arr: arr.map(row => row.join('')) })
+
+    return around.some(Boolean)
+  }
+
+  const checkDirectionChange = () => {
+    const touchedBoundary = {
+      right: j === size - 1,
+      left: j === 0,
+      bottom: i === size - 1,
+      up: i === 0,
+    }
+
+    if (direction === 'right') {
+      return touchedBoundary.right
+        || checkPassed(i, j + 1)
+        || checkPassed(i, j + 2)
+    }
+
+    if (direction === 'left') {
+      return touchedBoundary.left
+        || checkPassed(i, j - 1)
+        || checkPassed(i, j - 2)
+    }
+
+    if (direction === 'bottom') {
+      return touchedBoundary.bottom
+        || checkPassed(i + 1, j)
+        || checkPassed(i + 2, j)
+    }
+
+    if (direction === 'up') {
+      return touchedBoundary.up
+        || checkPassed(i - 2, j)
+        || checkPassed(i - 2, j)
+    }
+  }
+
+  const directionChange = () => {
+    if (direction === 'right') return direction = 'bottom'
+    if (direction === 'bottom') return direction = 'left'
+    if (direction === 'left') return direction = 'up'
+    if (direction === 'up') return direction = 'right'
+  }
+
+  const move = (getNewPosition) => {
+    const needEnd = checkEnd()
+
+    if (needEnd) {
+      direction = null
+      return
+    }
+
+    setPassed(i, j)
+    callback.call(this, i, j)
+
+    const newPosition = getNewPosition(i, j)
+    i = newPosition[0]
+    j = newPosition[1]
+
+    const needDirectionChange = checkDirectionChange()
+
+    if (needDirectionChange) {
+      directionChange()
+    }
+  }
+
+  while (direction) {
+    if (direction === 'right') move((i, j) => [i, j + 1])
+    if (direction === 'bottom') move((i, j) => [i + 1, j])
+    if (direction === 'left') move((i, j) => [i, j - 1])
+    if (direction === 'up') move((i, j) => [i - 1, j])
+  }
+
+  return arr
+}
